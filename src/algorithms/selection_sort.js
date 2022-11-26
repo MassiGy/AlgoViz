ad4ed2d63f5a587b18de4ba4d64a85b56e304f18
JugurtaO <jugurtaourzik@gmail.com>
2022-11-26 22:54:34 +0100

    selectionSort function implemented

--- src/algorithms/selection_sort.js
+++ src/algorithms/selection_sort.js
@@ -1,5 +1,22 @@
-module.exports.selection_sort_sort = (list) => {
-    
-};
+function sortBySelection(list) {
+    for (let i = 0; i < list.length-1; i++){
+        let min = list[i];
+        let minIndex=i;
+        for (let j = i + 1; j < list.length; j++){
+            if (list[j] < min){
+                min = list[j];
+                minIndex = j;
+            }
+        }
 
 
+        list[minIndex] = list[i];
+        list[i] = min;
+        
+    }
+
+
+
+
+    return list;
+}
