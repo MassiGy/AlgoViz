29f9e52512180f2088b781ca3f1f35c81eecb4d5
MassiGy <ghernaoutmassi@gmail.com>
2022-11-29 10:32:36 +0100

    +theme-implementation: refactored code, start using ES6 modules

--- src/helpers/sleep.js
+++ src/helpers/sleep.js
@@ -1,2 +1,5 @@
-module.exports.sleep = (millis) =>
-new Promise((resolve) => setTimeout(resolve, millis));
+const sleep = (millis) =>
+    new Promise((resolve) => setTimeout(resolve, millis));
+
+
+export default sleep;
--- src/views/script.js
+++ src/views/script.js
@@ -1,3 +1,6 @@
+import bubble_sort from "../algorithms/bubble_sort.js";
+
+
 let default_height = 600;
 let default_width = 600;
 
@@ -31,7 +34,6 @@ list.forEach((el, index) => {
 })
 
 
-console.log(bars.children);
 
 
 let launcher = document.getElementById("launcher")
@@ -39,46 +41,6 @@ let launcher = document.getElementById("launcher")
 launcher.addEventListener("click", () => bubble_sort(list, bars))
 
 
-const sleep = (millis) =>
-    new Promise((resolve) => setTimeout(resolve, millis));
-
-
-async function bubble_sort(list, bars) {
-    let val;
-    let temp = 0;
-    let fst;
-    let snd;
-    for (let i = 0; i < list.length; i++) {
-        for (let j = 0; j < list.length - 1; j++) {
-
-            if (list[j] > list[j + 1]) {
-
-                temp = list[j];
-                list[j] = list[j + 1];
-                list[j + 1] = temp;
-
-                // get the j'th element at first
-                fst = bars.children.namedItem(String(j));
-                // get the (j+1)'th element at second
-                snd = bars.children.namedItem(String(j + 1))
-
-                // swap their transform css proprety values
-                val = fst.style.transform;
-                fst.style.transform = snd.style.transform;
-                snd.style.transform = val;
-
-                // swap their ids, since id is thier index on the outer div (canvas) 
-                temp = fst.getAttribute("id");
-                fst.setAttribute("id", snd.getAttribute("id"));
-                snd.setAttribute("id", temp);
-
-                // sleep 2s, since we are swapping two elements, and foreach of them the transition is 1s
-                await sleep(2000);
-
-            }
-        }
-    }
-}
 
 
 
