c725bd72a19f4359e4351152426dc6f8374209f3
JugurtaO <jugurtaourzik@gmail.com>
2022-11-30 22:24:37 +0100

    +theme-implementation : redesigned page

--- src/views/script.js
+++ src/views/script.js
@@ -2,17 +2,17 @@ import bubble_sort from "../algorithms/bubble_sort.js";
 import selection_sort from "../algorithms/selection_sort.js";
 
 
-let default_height = 600;
+let default_height = 450;
 let default_width = 600;
 
 
 let bars = document.getElementById("bars");
-bars.style = `width: ${default_width}px; 
-              height: ${default_height}px;
-              margin: auto auto;
-              background-color: grey;
-              position: relative;
-              `;
+// bars.style = `width: ${default_width}px; 
+//               height: ${default_height}px;
+//               margin: auto auto;
+//               background-color: grey;
+//               position: relative;
+//               `;
 
 let list = [9, 1, 2, 4, 7, 5];
 let item = null;
@@ -26,7 +26,7 @@ list.forEach((el, index) => {
             position: absolute;
             transform: translate(${parseFloat(index * default_width / list.length)}px, 0%);
             transition: 1s ease-in-out;
-            background-color: red;
+            background-color: grey;
     `;
     // set an id so as we can refrence it back, 
     // it is important that these ids are in synch with the ids on the list
